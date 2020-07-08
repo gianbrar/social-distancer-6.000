85412fcc8793e4652d8df2d20825cc0aac096d60
thehiddenmonkey <gian.s.brar@gmail.com>
2020-07-07 20:39:09 -0400

    Added factorial nonsense

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
 import cv2
 import time
+import math
+
+def detect(faces):
+    if faces == 1:
+        return
 
 faceCascade = cv2.CascadeClassifier("haarcascade_frontalface_default.xml")
 camera = cv2.VideoCapture(0)
@@ -8,10 +13,6 @@ while True:
     return_value, image = camera.read()
     cv2.imwrite('working_image.png', image)
     image = cv2.imread('working_image.png')
-
-
-
-
     gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
     faces = faceCascade.detectMultiScale(
         gray,
@@ -25,6 +26,6 @@ while True:
     # Draw a rectangle around the faces
     for (x, y, w, h) in faces:
         cv2.rectangle(image, (x, y), (x+w, y+h), (0, 255, 0), 2)
-
     cv2.imwrite('worked_image.png', image)
+    detect(math.factorial(len(faces)) / (len(faces) - 2))
     time.sleep(1)
