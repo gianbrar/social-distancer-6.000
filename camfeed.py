d754da68d31164139836a58fe93faa2437a0a8b3
Blckbrry-Pi <54462713+Blckbrry-Pi@users.noreply.github.com>
2020-07-07 20:06:23 -0400

    Make camfeed.py kind of work

--- camfeed.py
+++ camfeed.py
@@ -1,7 +1,30 @@
 import cv2
+import time
 
+faceCascade = cv2.CascadeClassifier("haarcascade_frontalface_default.xml")
 camera = cv2.VideoCapture(0)
-for i in range(10):
+
+while True:
     return_value, image = camera.read()
-    cv2.imwrite('opencv'+str(i)+'.png', image)
-del(camera)
+    cv2.imwrite('working_image.png', image)
+    image = cv2.imread('working_image.png')
+
+
+
+
+    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    faces = faceCascade.detectMultiScale(
+        gray,
+        scaleFactor=1.2,
+        minNeighbors=5,
+        minSize=(30, 30)
+    )
+
+    print("Found {0} faces!".format(len(faces)))
+
+    # Draw a rectangle around the faces
+    for (x, y, w, h) in faces:
+        cv2.rectangle(image, (x, y), (x+w, y+h), (0, 255, 0), 2)
+
+    cv2.imwrite('worked_image.png', image)
+    time.sleep(1)
